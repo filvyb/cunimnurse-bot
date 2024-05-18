eb857bc0a7cf92d7d7166986b3f7ca07dbaf8d0d
Filip Vybihal <101115161+filvyb@users.noreply.github.com>
2024-05-18 21:46:19 +0200

    Refactor media dedupe + bugfix

--- src/utils/media_dedupe.nim
+++ src/utils/media_dedupe.nim
@@ -12,15 +12,38 @@ import std/httpclient
 import ../db/queries
 import ../utils/logging as clogger
 
+proc process_media_file(file_path: string, attach: Attachment): Future[string] {.async.} =
+  var ffmpeg_out_tmp = await execProcess(fmt"ffmpeg -i {file_path} -vf 'blackdetect=d=0.05:pix_th=0.67' -an -f null - 2>&1 | grep blackdetect")
+  var ffmpeg_out = (ffmpeg_out_tmp.output, ffmpeg_out_tmp.exitcode)
+
+  var cut_time = ""
+
+  if ffmpeg_out[1] == 0:
+    var ffmpeg_out_split = ffmpeg_out[0].split({'\n', '\r'})
+          
+    if ffmpeg_out_split.len != 1:
+      var start_time = ffmpeg_out_split[1].splitWhitespace()[3].split({':'})[1]
+      if start_time == "0":
+        cut_time &= "-ss "
+        cut_time &= ffmpeg_out_split[1].splitWhitespace()[4].split({':'})[1] & " "
+
+  var ffmpeg_thumb_out_tmp = await execProcess(fmt"ffmpeg -i {file_path} {cut_time}-frames:v 1 /tmp/{attach.id}.png -y")
+  var ffmpeg_thumb_out = (ffmpeg_thumb_out_tmp.output, ffmpeg_thumb_out_tmp.exitcode)
+
+  if ffmpeg_thumb_out[1] != 0:
+    raise newException(CatchableError, "Failed to process media file")
+
+  return "/tmp/" & attach.id & ".png"
+
 proc dedupe_media*(guild_id, channel_id, message_id: string, attach: Attachment): Future[(bool, int, string)] {.async.} =
   var file_path: string
 
   if attach.content_type.isSome:
-    let ext = attach.url.rsplit(".", 1)[1]
+    var client = newAsyncHttpClient()
+    let ext = attach.url.rsplit(".", 1)[1].split("?")[0]
     file_path = "/tmp/" & attach.id & "." & ext
     if attach.content_type.get() in ["image/jpeg", "image/png"]:
       try:
-        var client = newAsyncHttpClient()
         await client.downloadFile(attach.url, file_path)
 
       except CatchableError as e:
@@ -28,31 +51,9 @@ proc dedupe_media*(guild_id, channel_id, message_id: string, attach: Attachment)
         return (false, 0, "")
     if attach.content_type.get() in ["video/mp4", "video/ogg", "video/webm", "image/gif", "image/webp"]:
       try:
-        var client = newAsyncHttpClient()
         await client.downloadFile(attach.url, file_path)
 
-
-        var ffmpeg_out_tmp = await execProcess(fmt"ffmpeg -i {file_path} -vf 'blackdetect=d=0.05:pix_th=0.67' -an -f null - 2>&1 | grep blackdetect")
-        var ffmpeg_out = (ffmpeg_out_tmp.output, ffmpeg_out_tmp.exitcode)
-
-        var cut_time = ""
-
-        if ffmpeg_out[1] == 0:
-          var ffmpeg_out_split = ffmpeg_out[0].split({'\n', '\r'})
-          
-          if ffmpeg_out_split.len != 1:
-            var start_time = ffmpeg_out_split[1].splitWhitespace()[3].split({':'})[1]
-            if start_time == "0":
-              cut_time &= "-ss "
-              cut_time &= ffmpeg_out_split[1].splitWhitespace()[4].split({':'})[1] & " "
-
-        var ffmpeg_thumb_out_tmp = await execProcess(fmt"ffmpeg -i {file_path} {cut_time}-frames:v 1 /tmp/{attach.id}.png -y")
-        var ffmpeg_thumb_out = (ffmpeg_thumb_out_tmp.output, ffmpeg_thumb_out_tmp.exitcode)
-
-        if ffmpeg_thumb_out[1] != 0:
-          return (false, 0, "")
-
-        file_path = "/tmp/" & attach.id & ".png"
+        file_path = await process_media_file(file_path, attach)
 
       except CatchableError as e:
         error(e.msg)
